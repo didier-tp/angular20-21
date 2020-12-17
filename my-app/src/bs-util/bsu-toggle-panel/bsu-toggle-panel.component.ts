fb8f064aea98501bf8de41b50bd7475303746382
didier <didier@d-defrance.fr>
2020-12-17 11:25:27 +0100

    toogle-panel

--- my-app/src/bs-util/bsu-toggle-panel/bsu-toggle-panel.component.ts
+++ my-app/src/bs-util/bsu-toggle-panel/bsu-toggle-panel.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bsu-toggle-panel',
+  selector: 'bsu-toggle-panel',
   templateUrl: './bsu-toggle-panel.component.html',
   styleUrls: ['./bsu-toggle-panel.component.scss']
 })
 export class BsuTogglePanelComponent implements OnInit {
+
+  toggleP : boolean = false;
+
+  @Input()
+  title : string = "default-panel-title";
 
   constructor() { }
 
