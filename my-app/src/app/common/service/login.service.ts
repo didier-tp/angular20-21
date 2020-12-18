8b53c5f384b6bbf977f7e3a54aede59356348dc0
didier <didier@d-defrance.fr>
2020-12-18 15:04:00 +0100

    avec solution tp2

---
+++ my-app/src/app/common/service/login.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login } from '../data/login';
+import { LoginResponse } from '../data/login-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private _apiBaseUrl ="./login-api"; 
+  // with prefix in proxy.conf.json 
+  // (ng serve --proxy-config proxy.conf.json)
+  // or other config in production mode
+
+  private _headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private _http : HttpClient) { }
+
+  public postLogin$(login: Login): Observable<LoginResponse>{
+    let wsUrl  = this._apiBaseUrl +  "/public/auth" ; 
+    return this._http.post<LoginResponse>(wsUrl ,
+                                          login,
+                                          {headers: this._headers} );
+   }
+}
--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Login } from '../common/data/login'
+import { LoginResponse } from '../common/data/login-response';
+import { LoginService } from '../common/service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +12,31 @@ export class LoginComponent implements OnInit {
 
   public login : Login = new Login();
   public message :string ;
-  public onLogin(){
-     this.message = "donnees saisies = " + JSON.stringify(this.login);
+  public loginOk : boolean ;
+
+  public gererError(err :any){
+    this.message = "Une erreur technique a eu lieu. Veuillez réessayer plus tard";
+    this.loginOk=false;
+    console.log(JSON.stringify(err));
   }
 
-  constructor() { }
+  public gererLoginResponse(resp :LoginResponse){
+    this.message = resp.message;
+    this.loginOk = resp.status;
+    console.log(JSON.stringify(resp));
+  }
+
+  public onLogin(){
+     //this.message = "donnees saisies = " + JSON.stringify(this.login);
+     this._loginService.postLogin$(this.login).subscribe(
+      {
+        next : (resp :LoginResponse) => { this.gererLoginResponse(resp);  } ,
+        error : (err) => { this.gererError(err);}
+       }
+     );
+  }
+
+  constructor(private _loginService : LoginService) { }
 
   ngOnInit(): void {
   }
