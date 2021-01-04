d91087ab50de379d4b9423c1a973ac319bffd79e
didier <didier@d-defrance.fr>
2021-01-04 15:29:30 +0100

    admin_devise CRUD

--- my-app/src/app/admin-devise/admin-devise.component.ts
+++ my-app/src/app/admin-devise/admin-devise.component.ts
@@ -13,13 +13,27 @@ export class AdminDeviseComponent implements OnInit {
   selectedDevise : Devise; //devise à supprimer ou à ajouter ou à modifier
   listeDevises : Devise[];//liste des devises (pour selection)
   message : string = "";
+  isNew : boolean = true;
+  confirmDelete :boolean = false;
 
   constructor(private _deviseService : DeviseService) { 
     this.selectedDevise = new Devise();
   }
 
+  raz(){
+    this.selectedDevise = new Devise();
+    this.isNew=true;
+    this.confirmDelete = false;
+  }
+
   onNouvelleDevise(){
-     this.selectedDevise = new Devise();
+     this.raz();
+     this.message = "nouvelle devise à saisir et ajouter"
+  }
+
+  onSelection(){
+    this.isNew=false;
+    this.message = "devise existante (modifiable)"
   }
 
   onActualiser(){
@@ -31,25 +45,36 @@ export class AdminDeviseComponent implements OnInit {
   }
   
 
-  onRecupToutesDevise(){
+  onAjouterDevise(){  
+    this._deviseService.addDeviseServerSide$(this.selectedDevise)
+    .subscribe(
+      ()=>{this.message="devise ajoutée coté serveur"; this.onActualiser(); } ,
+      (error) => { console.log(error);
+                   this.message = JSON.stringify(error); }
+     );  
   }
 
-  onAjouterDevise(){    
-  }
-
-  onModifierDevise(){    
+  onModifierDevise(){ 
+    this._deviseService.updateDeviseServerSide$(this.selectedDevise)
+    .subscribe(
+      ()=>{this.message="devise modifiée coté serveur"; } ,
+      (error) => { console.log(error);
+                   this.message = JSON.stringify(error); }
+     );   
   }
 
   onSupprimer(){
     this._deviseService.deleteDeviseServerSide$(this.selectedDevise.code)
     .subscribe(
-      ()=>{this.message="ok"; } ,
+      ()=>{this.message="devise " +  this.selectedDevise.code + " bien supprimée";
+           this.onActualiser();   this.raz(); } ,
       (error) => { console.log(error);
                    this.message = JSON.stringify(error); }
      );
   }
 
   ngOnInit(): void {
+    this.onActualiser();
   }
 
 }
