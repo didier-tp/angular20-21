9fff6f06f243e65bb7b6acaa5fb85300997b62a7
didier <didier@d-defrance.fr>
2021-01-05 09:48:58 +0100

    debut routing sous niveau

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminDeviseComponent } from './admin-devise/admin-devise.component';
 import { BasicComponent } from './basic/basic.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
+import { TvaComponent } from './basic/tva/tva.component';
 import { ConversionComponent } from './conversion/conversion.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -10,7 +12,13 @@ const routes: Routes = [
   { path: 'ngr-welcome', component: WelcomeComponent },
   { path: '', redirectTo: '/ngr-welcome', pathMatch: 'full'},
   { path: 'ngr-login', component: LoginComponent },
-  { path: 'ngr-basic', component: BasicComponent },
+  { path: 'ngr-basic', component: BasicComponent,
+   children: [
+      { path: 'tva', component: TvaComponent },
+      { path: 'calculatrice', component: CalculatriceComponent },
+      { path: '', redirectTo: 'tva', pathMatch: 'prefix'}
+    ]
+  },
   { path: 'ngr-conversion', component: ConversionComponent },
   { path: 'ngr-admin-devise', component: AdminDeviseComponent }
 ];
